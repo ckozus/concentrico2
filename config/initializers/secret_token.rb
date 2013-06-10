01d3e6a01ad80a030a56ec0d409401b15cac5a33
Carlos Kozuszko <carlos@insignia4u.com>
2013-06-09 21:59:15 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concentrico2::Application.config.secret_token = '1964eb5920c6b94b9d99a0f61a47b5b43e9945cb1e028956f67ea80fa76f92fd595d7e90a4daf58924126edb3ed9a1b2836c688d997880eb27090e4a13563d43'
